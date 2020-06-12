940693b1633fa7060fe39af7cd8489e0b712257e
Emma Searle <emmasearle.ux@gmail.com>
2020-06-12 12:47:38 +0100

    Revert "Update script.js"
    This reverts commit 327fa737d6de88092f723e6dd6fb23c071863b4c.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'navtoggle';
-  }
-}
+console.log("Welcome to my world!")
